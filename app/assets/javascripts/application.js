3d81b5031c0c6117844119f5e8bd61a8095e0edb
Dodie324 <drmoore324@gmail.com>
2015-04-11 09:44:41 -0400

    Initial commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,30 +1,21 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require gmaps
 //= require_tree .
 
+$(document).foundation();
 
 $(document).ready(function() {
+
     var map = new GMaps({
           div: '#map-canvas',
           lat: 42.360083,
           lng: -71.05888,
           zoom: 12
     });
-      debugger;
     $('#myform').submit(function(e){
+      debugger;
         e.preventDefault();
         GMaps.geocode({
               address: $('#address').val().trim(),
