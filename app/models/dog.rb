df6f9a153828506db421257605700032aa54f11f
Dodie324 <drmoore324@gmail.com>
2015-04-15 12:51:24 -0400

    Push updates

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,10 +2,10 @@ class Dog < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
-  validates :age, presences: true
+  validates :age, presence: true
   validates :breed, presence: true
   validates :size, presence: true, inclusion: { in: %w(small medium large giant) }
   validates :fixed, presence: true
   validates :health_condition, presence: true
-  validates_presence_of :user
+  validates :user
 end
