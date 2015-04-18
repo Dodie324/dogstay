cf0318f00e8bcc0528528ca69d4a43081f5121f3
Dodie324 <drmoore324@gmail.com>
2015-04-18 15:28:57 -0400

    Update edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,27 @@ class ApplicationController < ActionController::Base
       :price,
       :availability
     ]
+
+    devise_parameter_sanitizer.for(:account_update) << [
+      :first_name,
+      :last_name,
+      :phone_number,
+      :address1,
+      :address2,
+      :city,
+      :state,
+      :zipcode,
+      :headline,
+      :description,
+      :sitter,
+      :have_dogs,
+      :have_children,
+      :property_type,
+      :yard_type,
+      :image,
+      :price,
+      :availability
+    ]
   end
 
   protect_from_forgery with: :exception
