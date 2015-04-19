79111708f8125b3fd3ee1c97cc0965e188eee0a0
Dodie324 <drmoore324@gmail.com>
2015-04-19 16:54:37 -0400

    Marker functionality

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,18 +2,29 @@ $(function() {
   function initialize() {
     var mapOptions = {
       center: { lat: centerLocation.latitude, lng: centerLocation.longitude },
-      zoom: 11
+      zoom: 12
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 
-    sitterLocation.forEach(function(sitter) {
+    sitterMark = sitterLocation.forEach(function(sitter) {
       var marker = new google.maps.Marker({
           position: new google.maps.LatLng(sitter.latitude, sitter.longitude),
           map: map
       });
     });
+
+    $("#search-sitter-container").on("click", ".sitter-profile", function() {
+      map = new google.maps.Map(document.getElementById('map-canvas'));
+      var laLatLng = new google.maps.LatLng(sitterLocation[0].latitude, sitterLocation[0].longitude);
+        map.panTo(laLatLng);
+        map.setZoom(15);
+    });
+
+  sitterMark;
   }
 
   google.maps.event.addDomListener(window, 'load', initialize);
 });
+
+
