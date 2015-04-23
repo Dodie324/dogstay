d1af2373c9b0d67497b2a5b9044c3ad0417b557a
Dodie324 <drmoore324@gmail.com>
2015-04-23 15:26:14 -0400

    Update sitter profile

---
+++ app/assets/javascripts/sitter-map.js
@@ -0,0 +1,25 @@
+$(function() {
+  function initialize() {
+    var mapOptions = {
+      center: { lat: oneLocation.latitude, lng: oneLocation.longitude },
+      zoom: 15
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(oneLocation.latitude, oneLocation.longitude),
+        map: map
+    });
+
+
+    $("#search-sitter-container").on("click", ".sitter-profile", function() {
+      map = new google.maps.Map(document.getElementById('map-canvas'));
+      var laLatLng = new google.maps.LatLng(oneLocation[0].latitude, oneLocation[0].longitude);
+        map.panTo(laLatLng);
+        map.setZoom(15);
+    });
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+});
