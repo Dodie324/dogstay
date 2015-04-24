9c5367f4a0abb95df4f69b3dfdbc522ccbdc4825
Dodie324 <drmoore324@gmail.com>
2015-04-24 08:13:39 -0400

    Update clode climate

--- app/assets/javascripts/sitter-map.js
+++ app/assets/javascripts/sitter-map.js
@@ -1,10 +1,13 @@
+var oneLocation;
+var google;
+
 $(function() {
   function initialize() {
     var mapOptions = {
       center: { lat: oneLocation.latitude, lng: oneLocation.longitude },
       zoom: 15
     };
-    var map = new google.maps.Map(document.getElementById('map-canvas1'),
+    var map = new google.maps.Map(document.getElementById("map-canvas1"),
         mapOptions);
 
     var marker = new google.maps.Marker({
@@ -12,14 +15,16 @@ $(function() {
         map: map
     });
 
-
     $("#search-sitter-container").on("click", ".sitter-profile", function() {
-      map = new google.maps.Map(document.getElementById('map-canvas'));
-      var laLatLng = new google.maps.LatLng(oneLocation[0].latitude, oneLocation[0].longitude);
+      map = new google.maps.Map(document.getElementById("map-canvas1"));
+      var laLatLng = new google.maps.LatLng(
+        oneLocation[0].latitude,
+        oneLocation[0].longitude
+        );
         map.panTo(laLatLng);
         map.setZoom(15);
     });
   }
 
-  google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, "load", initialize);
 });
