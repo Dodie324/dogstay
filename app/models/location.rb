24946048c6b89c32dc3bf67d2ea9b6e3a5715525
Dodie324 <drmoore324@gmail.com>
2015-05-01 20:22:54 -0400

    Make minor updates

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,24 @@ class Location < ActiveRecord::Base
   geocoded_by :location
 
   validates :location, presence: true
+
+  def coordinates_for(location)
+    location = location.downcase.gsub(/\W/, "")
+    coordinates = Location.find_by(location: location)
+    if coordinates
+      coordinates
+    else
+      lat_lng = Geocoder.coordinates(location)
+      if lat_lng == nil
+        flash[:notice] = "Please submit a valid location"
+        return root_path
+      else
+        Location.create!(
+          location: location,
+          latitude: lat_lng[0],
+          longitude: lat_lng[1]
+        )
+      end
+    end
+  end
 end
